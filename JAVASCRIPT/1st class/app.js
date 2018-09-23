fb912b62958767b2a9b873333f396e141042ff62
abdul-majid-ashrafi <dear_majid01@yahoo.com>
2018-09-23 12:15:28 -0700

    first commit

---
+++ JAVASCRIPT/1st class/app.js
@@ -0,0 +1,123 @@
+// /** blank **/
+// alert();
+
+// /** strings **/
+// alert("SSUET"); // double quatation with string
+// alert('SSUET'); // single quatation with string
+// alert("123"); // double quatation with number
+// alert("Hello World!"); // double quatation with string
+
+// alert(     "Hello World!"     );
+
+// alert("Hello             World!");
+
+// /** numbers **/
+// alert(123);
+// alert(1.23);
+
+
+
+
+
+
+
+// /** variable declaration **/
+// var myAge, country, myName;
+// alert(country); // undefine
+
+
+
+
+
+
+
+
+// /** variable initialization **/
+// country = "Pakistan";
+// alert(country);
+
+
+
+
+
+
+
+
+// var studentName = 'ABC XYZ';
+
+// var password = "pakistan";
+
+// var rollNumber = "A123";
+// var registrationNumber = "A123";
+// var email = 123456;
+// alert(typeof email);
+// email = "pakistan@gmail.com";
+// alert(typeof email);
+
+
+
+
+
+
+// var 2;            //illegal
+// var 2things;      //illegal
+// var .things;      //illegal
+// var %things;      //illegal
+// var -things;      //illegal
+// var @things;      //illegal
+// var #things;      //illegal
+// var _things;      //legal
+// var $things;      //legal
+// var th4ings2;      //legal
+// var twoThings;    //legal
+// var TwoThings;    //legal
+// var Two2Things;   //legal
+
+
+
+
+
+
+
+
+
+
+
+//   Variables for Numbers, Math Expressions
+
+// var num = 2;
+// var anotherNum = 3.2;
+
+// alert(num);
+// alert(anotherNum);
+// alert(num + 3);
+
+
+/** add two numbers **/
+//   var newNumber   = 4 + 3; // num and more
+//   alert(newNumber);
+
+
+
+
+
+
+//   var age = 19;
+//   var newAge = age + 3;
+//   alert(newAge , typeof age);
+//   alert(typeof age);
+
+
+
+
+
+var myName,number;
+myName = " Majid ";
+number =   345678987;
+
+var render = "My Name is " + myName + " number is " + number;
+console.log(render)
+console.log(0)
+document.write("<h1>" + render + "</h1>")
+document.write("<h1>" + "render" + "</h1>")
+
