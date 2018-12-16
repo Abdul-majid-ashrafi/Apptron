195f455d23db6fe93f2488bd8b254c76b3e20dff
abdul-majid-ashrafi <dear_majid01@yahoo.com>
2018-12-16 01:03:34 -0800

    update file

--- REACTJS/class_3/src/App.js
+++ REACTJS/class_3/src/App.js
@@ -3,35 +3,17 @@ import './App.css';
 import { HomeComponent } from './components/home';
 import { LoginComponent } from './components/login';
 import { SignupComponent } from './components/signup';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          {/* <h1>This App          </h1>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/login/">Login</Link>
-              </li>
-              <li>
-                <Link to="/signup/">Signup</Link>
-              </li>
-            </ul>
-          </nav> */}
-
-          <Route path="/" exact component={HomeComponent} />
-          <Route path="/login/" component={LoginComponent} />
-          <Route path="/signup/" component={SignupComponent} />
-        </div>
-      </Router>
-    );
-  }
+const BasicRouting = () => {
+  return (
+    <Router>
+      <div>
+        <Route path="/" exact component={HomeComponent} />
+        <Route path="/login/" component={LoginComponent} />
+        <Route path="/signup/" component={SignupComponent} />
+      </div>
+    </Router>
+  )
 }
-
-export default App;
+export default BasicRouting;
