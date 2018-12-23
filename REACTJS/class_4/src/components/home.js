a29cf3303732d9985b708e14effa96b487331302
abdul-majid-ashrafi <dear_majid01@yahoo.com>
2018-12-23 02:01:09 -0800

    implement chat messages

--- REACTJS/class_4/src/components/home.js
+++ REACTJS/class_4/src/components/home.js
@@ -6,8 +6,20 @@ export class HomeComponent extends Component {
     constructor() {
         super();
         this.state = {
-            user: {}
+            user: {},
+            chat: []
         }
+
+        var chatMessages = [];
+        var chatRef = firebase.database().ref('chatMessages/');
+        chatRef.on('child_added', (snapshot) => {
+            const obj = snapshot.val();
+            obj.key = snapshot.key;
+            chatMessages.push(obj);
+            this.setState({ chat: chatMessages })
+        });
+
+
     }
 
     componentWillMount() {
@@ -26,7 +38,25 @@ export class HomeComponent extends Component {
             })
     }
 
+    sendMessage() {
+        const data = {
+            senderUid: this.state.user.uid,
+            sender: this.state.user.username,
+            message: this.refs.msg.value
+        }
+        firebase.database().ref("/chatMessages/").push(data)
+            .then(() => {
+                console.log("Send Success");
+            });
+    }
+
+
     render() {
+        const inputStyle = {
+            padding: "10px 5px",
+            margin: "10px 5px",
+            width: "50%"
+        }
         return (
             <div>
                 <Navbar>
@@ -44,6 +74,16 @@ export class HomeComponent extends Component {
                         </Navbar.Text>
                     </Navbar.Collapse>
                 </Navbar>
+
+                <ul>
+                    {this.state.chat.map((val, idx) => {
+                        return <li key={idx}>  <b>{val.sender}:</b> {val.message}  </li>
+                    })}
+                </ul>
+
+                <input type="text" ref="msg" style={inputStyle} />
+                <Button onClick={this.sendMessage.bind(this)}>Send</Button>
+
             </div>
         )
     }
